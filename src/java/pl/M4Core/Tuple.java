2660b98878e5829745b284a350feebc6a2461a7e
M4gnez-thxforstealingmyusername <118939186+M4gnez-thxforstealingmyusername@users.noreply.github.com>
2024-06-17 19:10:47 +0200

    created Tuple

---
+++ src/java/pl/M4Core/Tuple.java
@@ -0,0 +1,27 @@
+package pl.M4Core;
+
+public class Tuple <T1, T2> {
+    private T1 key;
+    private T2 value;
+
+    public Tuple(T1 key, T2 value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public T1 getKey() {
+        return key;
+    }
+
+    public T2 getValue() {
+        return value;
+    }
+
+    public void setKey(T1 key) {
+        this.key = key;
+    }
+
+    public void setValue(T2 value) {
+        this.value = value;
+    }
+}
